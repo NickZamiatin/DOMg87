8f04ca636cc82ffbce7793dbf5e6147908bb5223
Nikita Zamiatin <4406015@gmail.com>
2018-04-13 10:52:44 -0400

    Dom with Jen

---
+++ script.js
@@ -0,0 +1,10 @@
+// window.addEventListener("click" , function () {
+//     console.log("You here");
+//
+//
+// })
+ let button = document.querySelector("button")
+
+button.addEventListener("click", function () {
+  console.log("Hey Nick")
+})
